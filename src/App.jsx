43e44900de258244072aa5a9407966f8c076a729
mvinduwara <mvinduwara@gmail.com>
2026-04-26 21:42:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } fr
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio">
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
